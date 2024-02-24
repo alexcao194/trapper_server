47b3f85ca565ecd3b67729b140cf8dbd4a94a365
Cao Việt Đức <alexcao194@gmail.com>
2024-02-24 17:10:45 +0700

    add cors

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const { authRouter, profileRouter } = require("./routes/index");
+const cors = require("cors");
 
 const app = express();
 
 
 app.use(logger("dev"));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
