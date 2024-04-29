e4631e4b70fed06c15f36cc62357aa40c44f30d9
NoraKyz <longkold00@gmail.com>
2024-04-29 13:13:34 +0700

    ping

--- routes/static.route.js
+++ routes/static.route.js
@@ -4,4 +4,8 @@ const staticController = require("../controllers/static.controller");
 
 staticRoute.get("/hobbies", staticController.getHobbies);
 
+staticRoute.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
 module.exports = staticRoute;
